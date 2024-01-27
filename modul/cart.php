a63e85445d673673f1fa7bc38af0f9434e22a979
Cuong <118752652+cuong-tn@users.noreply.github.com>
2024-01-27 11:38:48 +0700

    Delete modul directory

--- modul/cart.php
+++
@@ -1,7 +0,0 @@
-<?php
-function loadall_thongke(){
-$sql ="select danhmuc.name as tendm,danhmuc.id as madm,count(sanpham.id) as countsp,min(sanpham.price) as minprice,max(sanpham.price) as maxprice,avg(sanpham.price) as avgprice from sanpham left join danhmuc on danhmuc.id=sanpham.iddm where 1 group by danhmuc.id order by danhmuc.id desc ";
-$listtk=pdo_query($sql);
-return $listtk;
-}
-?>
